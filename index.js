f884487e31f93c622010dffb916348c69a672617
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-09-02 09:57:05 -0400

    adding some css, renaming gifs

---
+++ index.js
@@ -0,0 +1,3 @@
+Dom.ctx.textAlign = "center";
+Dom.ctx.fillStyle = "green";
+Dom.ctx.fillRect(0, 0, 1500, 750);
--- src/Dom.js
+++ src/Dom.js
@@ -3,6 +3,7 @@ class Dom {
   static canvas = document.getElementById("canvas");
   static ctx = canvas.getContext("2d");
   static form = document.getElementById("form");
+  static stats = document.getElementById("stats");
   
 
 }
