d1e0a48a575055102af3c23d00c8c46e30a3c37d
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-09-02 11:08:56 -0400

    working on driver class

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 Dom.ctx.textAlign = "center";
-Dom.ctx.fillStyle = "green";
+Dom.ctx.fillStyle = "rgba(11,156,49,0.6";
 Dom.ctx.fillRect(0, 0, 1500, 750);
+
+
+
+//const image = new Image();
+//image.src = 'images/Vans/EastVanDriving.gif';
+//image.addEventListener('load', function() {
+//  Dom.ctx.drawImage(image, 1, 1);
+//}, false)
+//
+//const driverImage = new Image();
+//driverImage.src = 'images/Drivers/EastDriver.gif';
+//driverImage.addEventListener('load', function() {
+//  Dom.ctx.drawImage(driverImage, 1, 50);
+//}, false)
+//
+//const houseImage = new Image();
+//houseImage.src = 'images/Homes/HouseWestSelectedDog.gif';
+//houseImage.addEventListener('load', function() {
+//  Dom.ctx.drawImage(houseImage, 100, 0, 100, 100);
+//}, false)
