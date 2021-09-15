ecc2af985d04568ba8af58babe1484b2efcc686e
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-09-15 08:41:31 -0400

    removing unused user class

--- index.js
+++ index.js
@@ -28,24 +28,3 @@ async function orderedFunction(func, key, message, ...args) {
   }
 
 }
-
-
-
-
-
-//Dom.ctx.textAlign = "center";
-//const van = new Van(14, 326);
-//const firstLocation = new Location(level)
-//document.body.addEventListener("keydown", function(e) {
-//    let player = (van.isDriving ? van : Driver.all[0] )
-//    player.move(e.keyCode);
-//    if (!van.isDriving && player instanceof Driver) {
-//      van.buckleUp(player);
-//      House.all.forEach(house => house.makeDelivery(player))
-//    }
-//  firstLocation.stateChange = true
-//  firstLocation.paint()
-//})
-
-
-
